f3051c230d5782abadce731a58b0c34a1406363c
brendanmariano <bmariano@colgate.edu>
2025-02-05 18:02:31 -0500

    Finished but something with command line needs to be fixed
    Could do final check as well

--- Untitled.R
+++ Untitled.R
@@ -1,17 +1,15 @@
 library("stringr")
+library("jsonlite")
 storedM = list.dirs(path = "MUSIC", full.names = TRUE, recursive = TRUE)
 print(storedM)
-
 num = str_count(storedM,"/")
-num
 #Finds all of the album sub-directories
-storedM[which(num == 2)]
-album.dirs <- storedM[which(num == 2)]
+(album.dirs <- storedM[which(num == 2)])
 
 print(album.dirs)
 
 #num2 = str_count(stored_files)
-
+code.to.process = vector(mode = "character", length = 0)
 for (curr.album in album.dirs)
 {
   #Accumulates songs from current album
@@ -23,8 +21,12 @@ for (curr.album in album.dirs)
   #Iterates through each song within the current album
   for(curr.song in song.files)
   {
-    code.to.process = vector(mode = "character", length = 0)
-    comb1 = paste(curr.album, curr.song, sep="/")
+    print(curr.album)
+    
+    #Should I print the track name or track path?
+    #And what about the parenthesis
+    comb1 = paste( '"',curr.album, curr.song,'"', sep="/")
+    
     #Removes the .wav from the track name
     track.name = str_sub(curr.song, start = 1, end = -5)
     #Finds the track name (Not including artist or anything else)
@@ -40,14 +42,48 @@ for (curr.album in album.dirs)
     album.split = album.split[[1]]
     album.name = album.split[3]
     file.output = paste(curr.artist, album.name,track.name, sep = "-")
-    #Creates code to be processed and puts into .txt file
-    code.to.process = paste("streaming_extractor_music.exe", curr.song, file.output)
-    #Opens the file in append mode so that batfile doesn't write over itself
-    curr.file = file("batfile.txt", "a")
-    writeLines(code.to.process, curr.file, sep = "\n")
-    close(curr.file)
+    file.output = paste('"',file.output,".json", '"', sep = "")
+    #Creates vector of code to be processed
+    code.to.process = c(code.to.process, paste("streaming_extractor_music.exe", comb1, file.output, sep = " "))
   }
 }
+#Puts code into .txt file
+writeLines(text = code.to.process, con = "batfile.txt", sep = "\n")
 
+#4
+#Keeps track of whether a file is a Json (for second lab period)
+num.ofJson = str_count(storedM, ".json")
+files.ofJson = storedM[which(num.ofJson == 1)]
+
+#for(curr.song in files.ofJson)
+#{
+
+#Manual declaration 
+curr.song = "The Front Bottoms-Talon Of The Hawk-Au Revoir (Adios).json"
+
+song.split = str_split(curr.song,"-")
+song.split = song.split[[1]]
+artist.name = song.split[1]
+album.name = song.split[2]
+song.name = song.split[3]
+JSON.files = fromJSON(curr.song)
+print(JSON.files)
+#Finds lowlevel values
+JSON.lowlevel.files = JSON.files$lowlevel
+(average_loudness = JSON.lowlevel.files$average_loudness)
+(spectral_energy_mean = JSON.lowlevel.files$spectral_energy$mean)
+#Finds rhythm values
+(JSON.rhythm.files = JSON.files$rhythm)
+(danceability = JSON.rhythm.files$danceability)
+(bpm = JSON.rhythm.files$bpm)
+
+#Finds other values outside of lowlevel and rhythm lists
+(key = JSON.files$tonal$key_key)
+(key_scale = JSON.files$tonal$key_scale)
+(length = JSON.files$metadata$audio_properties$length)
+
+
+
+#}
 
 
